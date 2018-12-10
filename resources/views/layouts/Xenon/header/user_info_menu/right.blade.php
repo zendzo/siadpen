03d34489098492791b4c22aa28e9409fc307507d
zendzo <zendzzo@gmail.com>
2018-12-10 16:40:55 +0700

    Add logout button on xenon theme

--- resources/views/layouts/Xenon/header/user_info_menu/right.blade.php
+++ resources/views/layouts/Xenon/header/user_info_menu/right.blade.php
@@ -46,10 +46,13 @@
     </a>
   </li>
   <li class="last">
-    <a href="extra-lockscreen.html">
-      <i class="fa-lock"></i>
-      Logout
-    </a>
+  <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+    <i class="fa fa-lock"></i>  Logout
+  </a>
+
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+      {{ csrf_field() }}
+  </form>
   </li>
 </ul>
 </li>
